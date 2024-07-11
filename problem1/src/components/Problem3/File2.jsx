15b0358f48409ad5ebf0cb72f8ac2bdfc9871682
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-11 13:21:31 +0530

    js Probelm1

--- problem1/src/components/Problem3/File2.jsx
+++ problem1/src/components/Problem3/File2.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React ,{useContext} from 'react'
 import File3 from './File3'
+import { counterContext } from './contextAPI/context'
 
 const File2 = () => {
+
+    const value  = useContext(counterContext)
   return (
     <div>
-        File 2 and import file file 3 :  <span><File3  /></span>
+        File 2 and import file file 3 : <button onClick={()=>value.setfirst((count)=>(count+1))} >{value.first} </button>  <span><File3  /></span>
     </div>
   )
 }
