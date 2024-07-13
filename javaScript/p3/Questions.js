9f44d31b27b837dbea97c9ab6de686cb37a95917
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-13 11:59:09 +0530

    problem 3

---
+++ javaScript/p3/Questions.js
@@ -0,0 +1,78 @@
+//----- . Write a program to print all natural numbers from 1 to n
+
+// let a = Number(prompt("Enter your first Number :  "));
+// let b = Number(prompt("Enter your last Number : "));
+
+// for(let i = a; i<=b;  i++) {
+//     console.log(i);  // prints numbers from 1 to n
+// }
+
+// ---------------------------------------------------------------------------------------
+
+// 2. Write a program to print all natural numbers in reverse
+// let a = Number(prompt("Enter your first Number :  "));
+// let b = Number(prompt("Enter your last Number : "));
+
+// for(let i = a; i>=b;  i--) {
+//     console.log(i);  // prints numbers from 1 to n
+// }
+
+
+// ---------------------------------------------------------------------------------------
+//    3. Write a program to print tables
+    //   let a = Number(prompt("your Table number  :  "));
+    //     for(let i = 1; i<=10;  i++) {
+    //       console.log(a +  " * " +  i  +  " =  " + a * i  );  
+    //   }
+
+
+
+
+// ---------------------------------------------------------------------------------------
+//    4. Write a program to print all alphabets from a to z
+// let a = parseInt(prompt("Enter your ASCII number: "));
+// let b = parseInt(prompt("Enter your ASCII number: "));
+// for (let i = a; i <= b; i++) {
+//     console.log(String.fromCharCode(i));  
+// }
+
+
+
+// ---------------------------------------------------------------------------------------
+//    5. Write a program to print all even numbers between 1 to 100
+// let a = parseInt(prompt("Enter your first even number: "));
+// let b = parseInt(prompt("Enter your last even number: "));
+// for (let i = a; i <= b; i++) {
+//      if( i % 2 == 0){
+//          console.log(i);
+//      }
+     
+// }
+
+
+
+
+// ---------------------------------------------------------------------------------------
+//    6. Write a program to find sum of all even  numbers between 1 to n
+// let a = parseInt(prompt("Enter your first even number: "));
+// let b = parseInt(prompt("Enter your last even number: "));
+// let sum =0;
+// for (let i = a; i <= b; i++) {
+//      if( i % 2 == 0){
+//          sum+=i;
+//      }
+     
+// }
+// console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
